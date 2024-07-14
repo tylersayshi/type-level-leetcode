def6450ecacf13ec853036a899bb52e4832514d3
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-07-14 15:02:49 -0700

    spiral description

--- problems/spiral.ts
+++ problems/spiral.ts
@@ -1,5 +1,26 @@
 import type { Minus, Decrement, Increment, Max } from "flat-type-samurai";
 
+/*
+(-2, 3)-----(-1, 3)----( 0, 3)----( 1, 3)----( 2, 3)----( 3, 3)
+    |                                                       |
+    |                                                       |
+(-2, 2)     (-1, 2)----( 0, 2)----( 1, 2)-----( 2, 2)       .
+    |          |                                 |          .
+    |          |                                 |          .
+(-2, 1)     (-1, 1)    ( 0, 1)----( 1, 1)     ( 2, 1)
+    |          |           |         |           |
+    |          |           |         |           |
+(-2, 0)     (-1, 0)    ( 0, 0)    ( 1, 0)     ( 2, 0)
+    |          |                     |           |
+    |          |                     |           |
+(-2,-1)     (-1,-1)----( 0,-1)----( 1,-1)     ( 2,-1)
+    |                                            |
+    |                                            |
+(-2,-2)-----(-1,-2)----( 0,-2)----( 1,-2)-----( 2,-2)
+
+Given a positive integer N, return the N-th coordinate of the spiral above.
+*/
+
 type MovePos<
   Pos extends [number, number],
   Dir extends "up" | "right" | "down" | "left",
